31852c75eb9debb7541f203b361f13eef97a5e07
Redster11 <42357111+Redster11@users.noreply.github.com>
2021-10-11 02:33:06 -0700

    Added connectivity to MySQL
    can search for a user by email

--- VaroChallenge/src/main/java/com/Redster11/VaroChallenge/App.java
+++ VaroChallenge/src/main/java/com/Redster11/VaroChallenge/App.java
@@ -10,9 +10,7 @@ public class App {
       Connection conn = DriverManager.getConnection(myUrl, "root", "Root");
        // our SQL SELECT query. 
       // if you only need a few columns, specify them by name instead of using "*"
-      String query = "SELECT P.*, A.*, AP.Active " +
-      "FROM Address AS A, People AS P, PeopleToAddress AS AP" +
-      "WHERE ((P.UserID=AP.UserID) And (AP.AddressID=A.AddressID) And (P.Email='kkirtland123@gmail.com'));";
+      String query = "SELECT * FROM people";
 
       // create the java statement
       Statement st = conn.createStatement();
@@ -25,11 +23,42 @@ public class App {
       {
         String firstName = rs.getString("FirstName");
         String lastName = rs.getString("LastName");
-        Date dateCreated = rs.getDate("date");
-        Boolean active = rs.getBoolean("Active");
+        String email = rs.getString("email");
         
         // print the results
-        System.out.format("%s, %s, %s, %s\n", firstName, lastName, dateCreated, active);
+        System.out.format("%s, %s, %s\n", firstName, lastName, email);
+      }
+      
+
+      query = "SELECT * FROM address";
+      rs = st.executeQuery(query);
+      while (rs.next())
+      {
+        String line1 = rs.getString("line1");
+        String city = rs.getString("city");
+        String state = rs.getString("state");
+        String zip = rs.getString("zip");
+        String date = rs.getString("date");
+
+        System.out.format("%s, %s, %s, %s, %s\n", line1, city, state, zip, date);
+      }
+      String input = "bdoney2000@yahoo.com";
+      query = "SELECT people.FirstName, people.lastName, people.email, address.line1, address.line2, address.city, address.state, address.zip, peopletoaddress.active" +
+      " FROM people, address, peopletoaddress" +
+      " Where people.UserID = peopletoaddress.UserID AND address.addressID = peopletoaddress.addressID AND people.email = \"" + input + "\"";
+      rs = st.executeQuery(query);
+      while (rs.next())
+      {
+        String firstName = rs.getString("FirstName");
+        String lastName = rs.getString("LastName");
+        String email = rs.getString("email");
+        String line1 = rs.getString("line1");
+        String city = rs.getString("city");
+        String state = rs.getString("state");
+        String zip = rs.getString("zip");
+        String active = rs.getString("active");
+
+        System.out.format("%s, %s, %s, %s, %s, %s, %s, %s\n", firstName, lastName, email, line1, city, state, zip, active);
       }
       st.close();
     }
